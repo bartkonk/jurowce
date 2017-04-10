c51950962f0674e91b5ed4f9a9088ad71150f3c6
bart konk <blah@blah.com>
2017-04-10 13:03:18 +0200

    defaults added

--- functions.py
+++ functions.py
@@ -97,11 +97,9 @@ class Globs:
             self.x[index] = price
             self.y[index] = 0.0
             self.y1[index] = 0.0
-            koszty_nieruchomosci_netto = koszty_nieruchomosci
-            sprzedaz_nieruchomosci_netto = sprzedaz_nieruchomosci
             zysk = 0.0
             for d in range(self.n_dzialek):
-                zysk +=(sprzedaz_nieruchomosci_netto - (koszty_nieruchomosci_netto*(1 - self.spadek_kosztow[d])))
+                zysk +=(sprzedaz_nieruchomosci - (koszty_nieruchomosci*(1 - self.spadek_kosztow[d])))
             
             zysk -= self.koszty_globalne
             zysk -= self.oplata_adjacencka
@@ -160,6 +158,14 @@ class Globs:
         _sprzedaz_nieruchomosci = widgets.FloatSlider(min=self.sprzedaz_nieruchomosci[0], max=self.sprzedaz_nieruchomosci[1], step=self.sprzedaz_nieruchomosci[2])
         _optymalizacja_podatkowa = widgets.FloatSlider(min=self.optymalizacja_podatkowa[0], max=self.optymalizacja_podatkowa[1], step=self.optymalizacja_podatkowa[2])
         
+        _cena_bez_podzialu.value = 100
+        _przed_adjacencka_cena_za_metr.value = 90
+        _wzrost_wartosci.value = .45
+        _koszty_nieruchomosci.value = 300000
+        _sprzedaz_nieruchomosci.value = 700000
+        _optymalizacja_podatkowa.value = 0.0
+        
+        
         _cena_bez_podzialu.continuous_update = False
         _przed_adjacencka_cena_za_metr.continuous_update = False
         _wzrost_wartosci.continuous_update = False
@@ -212,13 +218,13 @@ class Globs:
         width_ = "60%"
         wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='-'))
         wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='-'))
-        wid_extra_output.append(widgets.Text(width = width_, disabled=True))
-        wid_extra_output.append(widgets.Text(width = width_, disabled=True))
-        wid_extra_output.append(widgets.Text(width = width_, disabled=True))
-        wid_extra_output.append(widgets.Text(width = width_, disabled=True))
+        wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='Nominalnie za metr +'+str(wid_params[1].value * wid_params[2].value)))
+        wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='Brutto '+str(wid_params[3].value * (1+self.VAT1))))
+        wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='Brutto '+str(wid_params[4].value * (1+self.VAT2))))
+        wid_extra_output.append(widgets.Text(width = width_, disabled=True, value='Mniej o '+str(self.dochodowy * wid_params[5].value)))
         
         def cena_rzeczoznawcy(change):
-            a = widgets.Text(disabled=True, value = 'Nominalnie za metr '+str(wid_params[2].value * change['new']))
+            a = widgets.Text(disabled=True, value = 'Nominalnie za metr +'+str(wid_params[2].value * change['new']))
             widgets.jsdlink((a, 'value'), (wid_extra_output[2], 'value'))
             
         def wzrost_wartosci(change):
@@ -243,14 +249,16 @@ class Globs:
         wid_params[4].observe(vat2, names='value')
         wid_params[5].observe(dochodowy_minus, names='value')
         
-        
         for i in range(len(wid_params)):
-            boxes.append(Box([wid_names[i],wid_params[i],wid_extra_output[i]],margin = "0px 0px 10px 20px",width = '25%')) 
+            boxes.append(Box([wid_names[i],wid_params[i],wid_extra_output[i]],margin = "0px 0px 10px 20px",width = '28%')) 
 
         vbox1 = HBox([boxes[0], boxes[1], boxes[2]])
         vbox2 = HBox([boxes[3], boxes[4], boxes[5]])     
         display(vbox1)
         display(vbox2)
         
+        self.plot(_cena_bez_podzialu.value, _przed_adjacencka_cena_za_metr.value, _wzrost_wartosci.value, _koszty_nieruchomosci.value,\
+        _sprzedaz_nieruchomosci.value, _optymalizacja_podatkowa.value)
         interactive(self.plot,cena_bez_podzialu = _cena_bez_podzialu, przed_adjacencka_cena_za_metr = _przed_adjacencka_cena_za_metr, \
         wzrost_wartosci = _wzrost_wartosci, koszty_nieruchomosci = _koszty_nieruchomosci, sprzedaz_nieruchomosci = _sprzedaz_nieruchomosci, optymalizacja_podatkowa = _optymalizacja_podatkowa)
+        
